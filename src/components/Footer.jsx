62ee75c3dd55274892bd04a37780e43df1008c62
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-19 17:46:35 +0600

    build: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import classes from "../scss/Footer.module.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={classes.footer}>
+      <div>Movex</div>
+      <div>© 2022 Inc || All Rights Reserved </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
