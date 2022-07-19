6f0396120f4354ff58980ff504f030ef58f6c654
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-19 15:59:12 +0600

    build: all routes to root

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import MovieDetails from "./components/MovieDetails";
+import PageNotFound from "./components/PageNotFound";
 
 const Root = () => {
   return (
-    <div>Root</div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:imdbID" element={<MovieDetails />} />
+        <Route element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default Root
+export default Root;
