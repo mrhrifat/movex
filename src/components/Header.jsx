959a8ade5eb274730f6120b81d26a4f3de5be604
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-19 17:13:09 +0600

    build: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import user from "../images/user.png";
+import { Link } from "react-router-dom";
+import classes from "../scss/Header.module.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className={classes.header}>
+      <Link to="/">
+        <div className={classes.logo}>Movex</div>
+      </Link>
+      <div className={classes.userImage}>
+        <img src={user} alt="User" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
