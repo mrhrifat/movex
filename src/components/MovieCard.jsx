a88bcb51191728001ffb7d2ddb503baf08447fe5
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-20 15:41:44 +0600

    build: movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import classes from "../scss/MovieCard.module.css";
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ data }) => {
+  // console.log(data);
+  const { Poster, Title, Year } = data;
+  return (
+    <div className={classes.cardItem}>
+      <div className={classes.cardInner}>
+        <div className={classes.cardTop}>
+          <img src={Poster} alt={Title} />
+        </div>
+        <div className={classes.cardBottom}>
+          <div className={classes.cardInfo}>
+            <h4>{Title}</h4>
+            <p>{Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
