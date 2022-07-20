73b50b68c52905f0d439086689e4d066e29b8733
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-20 17:38:39 +0600

    update reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import moviesReducer from "./movies/movieSlice";
+import moviesReducer from "./slices/movieSlice";
+import seriesReducer from "./slices/seriesSlice";
 
 const store = configureStore({
   reducer: {
     movies: moviesReducer,
+    series: seriesReducer,
   },
 });
 export default store;
