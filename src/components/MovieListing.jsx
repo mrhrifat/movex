5dc531aafc85d615a42621e74fb30b1508460c56
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-20 14:58:11 +0600

    build: movie listing & render with movie card

--- src/components/MovieListing.jsx
+++ src/components/MovieListing.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../redux/movies/movieSlice";
+import MovieCard from "./MovieCard";
+import classes from "../scss/MovieListing.module.css";
 
 const MovieListing = () => {
-  return <div>MovieListing</div>;
+  const movies = useSelector(getAllMovies);
+
+  const renderList =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => {
+        return <MovieCard key={index} data={movie} />;
+      })
+    ) : (
+      <div className={classes.moviesError}>
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+
+  return (
+    <div className={classes.movieWrapper}>
+      <div className={classes.movieList}>
+        <h2>Movies</h2>
+        <div className={classes.movieContainer}>{renderList}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
