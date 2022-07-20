b83df7e625aa2c486625ef5a70cdbd778dca8084
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-20 15:54:46 +0600

    build: async dispatch of data fetching

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,35 @@
 import React, { useEffect } from "react";
 import MovieListing from "./MovieListing";
 import { useDispatch } from "react-redux";
-import movieApi from "../api/movieApi";
-import { addMovies } from "../redux/movies/movieSlice";
+import { addMovies, fetchMovies } from "../redux/movies/movieSlice";
 
 const Home = () => {
-  const movieTextKey = "titanic";
-  const type = "movie";
+  // const movieTextKey = "harry";
+  // const type = "movie";
 
   const dispatch = useDispatch();
 
-  const fetchData = async () => {
-    return await movieApi
-      .get(
-        `?apiKey=${process.env.REACT_APP_API_KEY}&s=${movieTextKey}&type=${type}`
-      )
-      .then((res) => {
-        // console.log(res);
-        dispatch(addMovies(res.data));
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+  // const fetchData = async () => {
+  //   return await movieApi
+  //     .get(
+  //       `?apiKey=${process.env.REACT_APP_API_KEY}&s=${movieTextKey}&type=${type}`
+  //     )
+  //     .then((res) => {
+  //       // console.log(res);
+  //       dispatch(addMovies(res.data));
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   // dispatch(addMovies(fetchData));
+  //   fetchData();
+  // }, []);
 
   useEffect(() => {
-    // dispatch(addMovies(fetchData));
-    fetchData();
+    dispatch(fetchMovies());
   }, []);
 
   return (
