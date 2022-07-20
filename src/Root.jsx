14a8ff3719d2d2d5e55af5596da4758f89c8e5f0
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-20 15:01:02 +0600

    build: update classes name

--- src/Root.jsx
+++ src/Root.jsx
@@ -5,12 +5,13 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import MovieDetails from "./components/MovieDetails";
 import PageNotFound from "./components/PageNotFound";
+import classes from "./scss/All.css";
 
 const Root = () => {
   return (
     <BrowserRouter>
       <Header />
-      <div className="container">
+      <div className={classes.container}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movie/:imdbID" element={<MovieDetails />} />
