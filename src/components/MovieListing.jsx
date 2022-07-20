0fd84f38bb2c88dc752a66091db155d1310a1d4e
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-20 17:41:03 +0600

    build: render series list

--- src/components/MovieListing.jsx
+++ src/components/MovieListing.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../redux/movies/movieSlice";
+import { getAllMovies } from "../redux/slices/movieSlice";
 import MovieCard from "./MovieCard";
 import classes from "../scss/MovieListing.module.css";
+import { getAllSeries } from "../redux/slices/seriesSlice";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  const series = useSelector(getAllSeries);
+
+  // console.log(movies);
+  // console.log(series);
 
   const renderList =
     movies.Response === "True" ? (
@@ -14,7 +19,20 @@ const MovieListing = () => {
       })
     ) : (
       <div className={classes.moviesError}>
-        <h3>{movies.Error}</h3>
+        {/* <h3>{movies.Error}</h3> */}
+        <h3>Error</h3>
+      </div>
+    );
+
+  const renderSeriesList =
+    series.Response === "True" ? (
+      series.Search.map((series, index) => {
+        return <MovieCard key={index} data={series} />;
+      })
+    ) : (
+      <div className={classes.moviesError}>
+        {/* <h3>{series.Error}</h3> */}
+        <h3>Error</h3>
       </div>
     );
 
@@ -23,6 +41,10 @@ const MovieListing = () => {
       <div className={classes.movieList}>
         <h2>Movies</h2>
         <div className={classes.movieContainer}>{renderList}</div>
+      </div>
+      <div className={classes.showList}>
+        <h2>Series</h2>
+        <div className={classes.movieContainer}>{renderSeriesList}</div>
       </div>
     </div>
   );
