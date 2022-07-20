793096d8e82856f2583f7dde7a9c720465a2f340
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-20 14:57:33 +0600

    build: fetch data from server & dispatch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MovieListing from "./MovieListing";
+import { useDispatch } from "react-redux";
+import movieApi from "../api/movieApi";
+import { addMovies } from "../redux/movies/movieSlice";
 
 const Home = () => {
+  const movieTextKey = "titanic";
+  const type = "movie";
+
+  const dispatch = useDispatch();
+
+  const fetchData = async () => {
+    return await movieApi
+      .get(
+        `?apiKey=${process.env.REACT_APP_API_KEY}&s=${movieTextKey}&type=${type}`
+      )
+      .then((res) => {
+        // console.log(res);
+        dispatch(addMovies(res.data));
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    // dispatch(addMovies(fetchData));
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div className="bannerImg"></div>
