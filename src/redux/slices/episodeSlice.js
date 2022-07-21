8344b660debbb0cbab7cd5aedef04c5de48e05be
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-21 11:51:11 +0600

    build: episode slice

---
+++ src/redux/slices/episodeSlice.js
@@ -0,0 +1,46 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from "../../api/movieApi";
+
+const initialState = {
+  episodes: {},
+};
+
+const fetchEpisodes = createAsyncThunk("episodes/fetchEpisodes", async () => {
+  const episodeTextKey = "hacker";
+  //   const type = "episode";
+  // Episode Type Not Working
+  const type = "movie";
+
+  return await movieApi
+    .get(
+      `?apiKey=${process.env.REACT_APP_API_KEY}&s=${episodeTextKey}&type=${type}`
+    )
+    .then((res) => {
+    //   console.log(res);
+      return res.data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
+
+const episodeSlice = createSlice({
+  name: "episodes",
+  initialState,
+  extraReducers: {
+    [fetchEpisodes.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchEpisodes.fulfilled]: (state, { payload }) => {
+      console.log("Successful");
+      return { ...state, episodes: payload };
+    },
+    [fetchEpisodes.rejected]: () => {
+      console.log("Rejected");
+    },
+  },
+});
+
+export default episodeSlice.reducer;
+export { fetchEpisodes };
+export const getAllEpisodes = (state) => state.episodes.episodes;
