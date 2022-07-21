a400f9bd7f864c4b2148113def25dd093f8bbda0
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-22 03:23:23 +0600

    reduce space

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,6 @@ const Header = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     // console.log(term);
-    
     if (term === "") return alert("Please enter search term!");
     dispatch(fetchMovies(term));
     dispatch(fetchSeries(term));
