71d58b436c4b7ebf1a241a0ff1a77a668432439a
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-21 12:43:37 +0600

    feat: wrap with link

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import classes from "../scss/MovieCard.module.css";
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ data }) => {
   // console.log(data);
-  const { Poster, Title, Year } = data;
+  const { Poster, Title, Year, imdbID } = data;
   return (
-    <div className={classes.cardItem}>
-      <div className={classes.cardInner}>
-        <div className={classes.cardTop}>
-          <img src={Poster} alt={Title} />
-        </div>
-        <div className={classes.cardBottom}>
-          <div className={classes.cardInfo}>
-            <h4>{Title}</h4>
-            <p>{Year}</p>
+    <Link to={`/movie/${imdbID}`}>
+      <div className={classes.cardItem}>
+        <div className={classes.cardInner}>
+          <div className={classes.cardTop}>
+            <img src={Poster} alt={Title} />
+          </div>
+          <div className={classes.cardBottom}>
+            <div className={classes.cardInfo}>
+              <h4>{Title}</h4>
+              <p>{Year}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
