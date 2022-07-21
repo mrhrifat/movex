f438177cb2efd3f0437b1d5f5deaec2e45520cd3
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-21 16:50:17 +0600

    build: search bar  & update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import user from "../images/user.png";
 import { Link } from "react-router-dom";
 import classes from "../scss/Header.module.css";
@@ -8,27 +8,37 @@ import Navbar from "react-bootstrap/Navbar";
 import { Form, Button } from "react-bootstrap";
 
 const Header = () => {
+  const [term, setTerm] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(term);
+  };
+
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Link to="/">
-            <Navbar.Brand href="#home">Movex</Navbar.Brand>
-          </Link>
+          <Navbar.Brand>
+            <Link to="/">Movex</Link>
+          </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="responsive-navbar" />
 
           <Navbar.Collapse id="responsive-navbar">
             <Nav className="m-auto">
-              {/* <Nav.Link href="#features">Features</Nav.Link> */}
-              {/* <Nav>Features</Nav> */}
-              <Form className="d-flex">
+              <Form className="d-flex" onSubmit={submitHandler}>
                 <Form.Control
                   type="search"
-                  placeholder="Search"
+                  placeholder="Search..."
                   className="me-2"
                   aria-label="Search"
+                  value={term}
+                  onChange={(e) => setTerm(e.target.value)}
                 />
-                <Button variant="outline-success">Search...</Button>
+                <Button variant="outline-success" type="submit">
+                  Search
+                </Button>
               </Form>
             </Nav>
 
