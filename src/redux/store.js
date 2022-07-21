9ef1a967c5d55048295cca54543bad12fa2d0dd5
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-21 11:51:27 +0600

    build: episode reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./slices/movieSlice";
 import seriesReducer from "./slices/seriesSlice";
+import episodeReducer from "./slices/episodeSlice";
 
 const store = configureStore({
   reducer: {
     movies: moviesReducer,
     series: seriesReducer,
+    episodes: episodeReducer,
   },
 });
 export default store;
